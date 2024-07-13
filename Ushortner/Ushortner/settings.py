94151e3c28f4ae18f31bdea0afa89c2a42ceec01
Bright <nanabrightatsiatorme@gmail.com>
2024-07-13 04:00:45 +0000

    trying second deploy

--- Ushortner/Ushortner/settings.py
+++ Ushortner/Ushortner/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
     }
 }
 
-# DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
+DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
