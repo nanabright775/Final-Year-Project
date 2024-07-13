08b59756a6e70f6093709ee6ceee4bd58059fc15
Bright Atsiatorme <81465482+nanabright775@users.noreply.github.com>
2024-07-13 03:58:28 +0000

    Delete ushortner directory

--- ushortner/business_card/admin.py
+++
@@ -1,7 +0,0 @@
-# business_card/admin.py
-
-from django.contrib import admin
-from .models import BusinessCardTemplate, UserBusinessCard
-
-admin.site.register(BusinessCardTemplate)
-admin.site.register(UserBusinessCard)
--- ushortner/user/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-def create_user(username, email, password):
-    user = User(username=username, email=email)
-    user.set_password(password)
-    user.save()
-    return user
